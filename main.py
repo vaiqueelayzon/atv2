28cb718785aa9c6a4d3d847f7d9b972fa8dcdd1f
vaiqueelayzon <layzon.nicolas08@aluno.ifce.edu.br>
2025-08-25 13:47:32 +0000

    descrição das alterações realizadas

--- main.py
+++ main.py
@@ -3,3 +3,12 @@ app = FastAPI(title='API do layzon')
 @app.get("/")
 def hello():
     return{"message": "Hello Word!"}
+
+@app.get("/aluno")
+def get_aluno():
+    return{"insira na rota após a '/' o nome do aluno"}
+
+@app.get("/aluno/{nome_aluno}")
+def get_aluno_by_name(nome_aluno: str):
+    return{"o nome do aluno é": nome_aluno}
+
